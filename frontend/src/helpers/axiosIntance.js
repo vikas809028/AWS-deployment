5e256ddc5fdc168d56654849101e534f978df133
vikas809028 <vikastiwari809028@gmail.com>
2025-07-30 21:45:03 +0530

    fixed url

--- frontend/src/helpers/axiosIntance.js
+++ frontend/src/helpers/axiosIntance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5014/api';
+const BASE_URL = 'http://3.110.154.69:5014';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
